deb3f4a31a8ac2ea8ae07adbc332c0f7e98c8c99
oldratlee <oldratlee@gmail.com>
2013-02-15 17:50:08 +0800

    change absulute path to relative

--- update-index.sh
+++ update-index.sh
@@ -5,6 +5,6 @@ BASE=`pwd`
 
 (
 	cat head.txt ; 
-	(find com -type f | sed 's#^#http://metaframe.github.com/repo/#;s#.*#<a href="\0">\0</a><br/>#' ) ; 
+	(find com -type f | sed 's#.*#<a href="./\0">./\0</a><br/>#' ) ; 
 	cat tail.txt
 ) > index.html

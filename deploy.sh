acc5284238d32d745bca2de2ef47b1ce38ee7a32
oldratlee <oldratlee@gmail.com>
2013-02-17 00:57:29 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,17 @@
 cd `dirname $0`
 BASE=`pwd`
 
+if [ -z "$1" ] ; then
+	comment=update
+else
+	comment="$1"
+fi
+
 (
 	cat head.txt ; 
 	(find com -type f | sed 's#.*#<a href="./\0">./\0</a><br/>#' ) ; 
 	cat tail.txt
-) > index.html
+) > index.html &&
+git add . &&
+git commit -m "$comment" &&
+time git push
